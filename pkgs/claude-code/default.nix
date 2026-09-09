66f416244567f763f194075d57527cc1c04078e6
Corin Lawson <c.lawson@machship.com>
2026-09-09 11:24:09 +1000

    chore: bump claude-code 2.1.266

--- pkgs/claude-code/default.nix
+++ pkgs/claude-code/default.nix
@@ -8,7 +8,7 @@ let
   # See DOWNLOAD_BASE_URL in https://claude.ai/install.sh
   dbu = "https://downloads.claude.ai/claude-code-releases";
 
-  version = "2.1.263";
+  version = "2.1.266";
 
   # hash when bumping.
   #   nix-prefetch-url "${dbu}/$(curl -fsSL ${dbu}/latest)/manifest.json"
@@ -16,7 +16,7 @@ let
     builtins.readFile (
       builtins.fetchurl {
         url = "${dbu}/${version}/manifest.json";
-        sha256 = "0shi519jfsiiry4g3k972zsd1dxqih1a49l8029wmccqbfvlpxln";
+        sha256 = "1r2pahzvsxcyakplxx8h6d9ra51wq613082iicinr6z8rrbb8di7";
       }
     )
   );
